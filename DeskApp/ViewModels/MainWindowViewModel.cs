5bb768ab887e6dc6fd03cb07ee5f3ba416ecfb5c
AdilJavatogo <Adil2300@hotmail.com>
2026-01-06 06:30:09 +0100

    Integration

--- DeskApp/ViewModels/MainWindowViewModel.cs
+++ DeskApp/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,95 @@
-﻿namespace DeskApp.ViewModels
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using DeskApp.ViewModels;
+
+namespace DeskApp.ViewModels
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    // Hoved ViewModel for mødet
+    public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        private ParticipantViewModel _activeSpeaker;
+        private bool _isMuted;
+        private bool _isVideoOn = true;
+        private Timer _videoTimer;
+
+        public ObservableCollection<ParticipantViewModel> Participants { get; } = new();
+
+        public ParticipantViewModel ActiveSpeaker
+        {
+            get => _activeSpeaker;
+            set { _activeSpeaker = value; OnPropertyChanged(); }
+        }
+
+        public string IsMutedIcon => _isMuted ? "🔴" : "🎙️";
+        public string IsVideoOnIcon => _isVideoOn ? "📹" : "🚫";
+
+        public MainWindowViewModel()
+        {
+            // Opret dummy deltagere
+            Participants.Add(new ParticipantViewModel("Lars Larsen", Colors.Blue));
+            Participants.Add(new ParticipantViewModel("Mette Frederiksen", Colors.Red));
+            Participants.Add(new ParticipantViewModel("Ole Opfinder", Colors.Green));
+            Participants.Add(new ParticipantViewModel("Dig (Mig)", Colors.Purple) { IsVideoOn = true });
+
+            // Sæt den første som "Active Speaker" (den store skærm)
+            ActiveSpeaker = Participants[0];
+
+            // Start en "Fake Video Engine" for at simulere kamera-input
+            // I en rigtig app ville dette være WebRTC frames der kommer ind
+            _videoTimer = new Timer(SimulateIncomingVideoFrames, null, 0, 33); // Ca. 30 FPS
+        }
+
+        // --- Kommandoer til UI knapper ---
+        public void ToggleMuteCommand()
+        {
+            _isMuted = !_isMuted;
+            OnPropertyChanged(nameof(IsMutedIcon));
+        }
+
+        public void ToggleVideoCommand()
+        {
+            _isVideoOn = !_isVideoOn;
+            OnPropertyChanged(nameof(IsVideoOnIcon));
+
+            // Opdater også "Min" deltager i listen
+            var me = Participants[3];
+            me.IsVideoOn = _isVideoOn;
+        }
+
+        public void LeaveCallCommand()
+        {
+            Environment.Exit(0);
+        }
+
+        // --- SIMULERING AF VIDEO STREAM ---
+        // Dette er den vigtige del teknisk set. Vi skriver rå bytes til grafikkortet.
+        private void SimulateIncomingVideoFrames(object? state)
+        {
+            // Vi opdaterer kun UI på UI-tråden
+            Dispatcher.UIThread.Post(() =>
+            {
+                // Opdater Active Speaker (Stor skærm)
+                ActiveSpeaker?.UpdateVideoFrame();
+
+                // Opdater alle små billeder i bunden
+                foreach (var p in Participants)
+                {
+                    if (p.IsVideoOn) p.UpdateVideoFrame();
+                }
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
---
+++ DeskApp/ViewModels/ParticipantViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DeskApp.ViewModels
+{
+    // Model for en enkelt person i mødet
+    public class ParticipantViewModel : ViewModelBase
+    {
+        public string Name { get; }
+        public string Initials => Name.Substring(0, 1);
+        public IBrush AvatarColor { get; }
+
+        private bool _isVideoOn = true;
+        public bool IsVideoOn
+        {
+            get => _isVideoOn;
+            set { _isVideoOn = value; OnPropertyChanged(); }
+        }
+
+        // Dette er billedet vi viser i UI
+        private WriteableBitmap _videoFrame;
+        public WriteableBitmap VideoFrame
+        {
+            get => _videoFrame;
+            set { _videoFrame = value; OnPropertyChanged(); }
+        }
+
+        // Variabler til animation (kun for demo skyld)
+        private double _animOffset = 0;
+        private readonly Random _rnd = new Random();
+
+        public ParticipantViewModel(string name, Color color)
+        {
+            Name = name;
+            AvatarColor = new SolidColorBrush(color);
+
+            // Opret en tom bitmap (f.eks. 320x180 pixels for thumbnails)
+            // DPI 96 er standard
+            VideoFrame = new WriteableBitmap(new PixelSize(320, 180), new Vector(96, 96), PixelFormat.Bgra8888, AlphaFormat.Premul);
+        }
+
+        // Denne metode kaldes 30 gange i sekundet for at tegne nyt "kamera" billede
+        public unsafe void UpdateVideoFrame()
+        {
+            using (var buf = VideoFrame.Lock())
+            {
+                var ptr = (uint*)buf.Address;
+                int w = buf.Size.Width;
+                int h = buf.Size.Height;
+
+                // Demo Animation: En bevægende bar der skifter farve (Simulerer video)
+                _animOffset += 5;
+                if (_animOffset > w) _animOffset = 0;
+
+                // Fyld pixels (Simpel rendering loop)
+                for (int y = 0; y < h; y++)
+                {
+                    for (int x = 0; x < w; x++)
+                    {
+                        // Baggrundsfarve (Mørkegrå støj)
+                        uint color = 0xFF202020;
+
+                        // Tegn en bevægende stribe
+                        if (x > _animOffset && x < _animOffset + 20)
+                        {
+                            color = 0xFFFF0000; // Rød
+                        }
+
+                        // Skriv til hukommelsen
+                        ptr[y * w + x] = color;
+                    }
+                }
+            }
+            // Fortæl UI at dette billede skal tegnes igen
+            OnPropertyChanged(nameof(VideoFrame));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
